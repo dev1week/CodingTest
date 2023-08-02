d32c741f0df0031bbcc6969067c97b728ffab7a7
HanJu <119592507+dev1week@users.noreply.github.com>
2023-08-02 14:14:20 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 880 ms, Memory: 127784 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,44 +1,60 @@
-import java.io.*;
+import java.io.*; 
+
 import java.util.*; 
 
-public class Main{
-    public static void main(String[] args)throws IOException{
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokens = new StringTokenizer(buffer.readLine());
-        int size = Integer.valueOf(tokens.nextToken());
-        int queryNum = Integer.valueOf(tokens.nextToken());
-        int[][] a = new int[size][size];
-        int[][] s = new int[size+1][size+1];
-        for(int y=0; y<size; y++){
-            tokens = new StringTokenizer(buffer.readLine());
-            for(int x=0; x<size; x++){
-                a[y][x] = Integer.valueOf(tokens.nextToken());
-            }
-        }
-
-        for(int x =1; x<=size; x++){
-            s[1][x] = s[1][x-1] + a[0][x-1];
-        }
-        for(int y=1; y<=size; y++){
-            s[y][1] = s[y-1][1] +a[y-1][0];
-        }
-        for(int y=2; y<=size; y++){
-            for(int x=2; x<=size; x++){
-                s[y][x] = s[y-1][x] +s[y][x-1] -s[y-1][x-1] +a[y-1][x-1];
-            }
-        }
-
-        for(int q =0; q<queryNum; q++){
-            tokens = new StringTokenizer(buffer.readLine());
-            int x1 = Integer.valueOf(tokens.nextToken());
-            int y1 = Integer.valueOf(tokens.nextToken());
-            int x2 =Integer.valueOf(tokens.nextToken());
-            int y2 =Integer.valueOf(tokens.nextToken());
-            System.out.println(s[x2][y2]-s[x1-1][y2]-s[x2][y1-1]+s[x1-1][y1-1]);
-        }
 
 
-    }
-
-
+class Main
+{
+	//입력 변수 정의 
+	static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens; 
+	
+	static int[][] num;
+	static int[][] partSum; 
+	
+	static int n; 
+	static int m; 
+	
+	static void init() throws IOException{
+		tokens = new StringTokenizer(buffer.readLine()); 
+		n = Integer.valueOf(tokens.nextToken()); 
+		m = Integer.valueOf(tokens.nextToken()); 
+		
+		num = new int[n+1][n+1]; 
+		partSum = new int[n+1][n+1]; 
+		
+		for(int x=1; x<=n; x++) {
+			tokens = new StringTokenizer(buffer.readLine());
+			for(int y=1; y<=n; y++) {
+				num[x][y] = Integer.valueOf(tokens.nextToken());
+			}
+		}
+		
+	}
+	
+	static void makePartSum() {
+		for(int x=1; x<=n; x++) {
+			for(int y=1;y<=n; y++ ) {
+				partSum[x][y] = num[x][y]+partSum[x-1][y]+partSum[x][y-1] -partSum[x-1][y-1];  
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+		init();
+		
+		makePartSum(); 
+		StringBuilder result = new StringBuilder(); 
+		for(int i=0; i<m; i++) {
+			tokens = new StringTokenizer(buffer.readLine());
+			int x1 = Integer.valueOf(tokens.nextToken());
+			int y1 = Integer.valueOf(tokens.nextToken());
+			int x2 = Integer.valueOf(tokens.nextToken());
+			int y2 = Integer.valueOf(tokens.nextToken());
+			
+			result.append(partSum[x2][y2]-partSum[x1-1][y2]-partSum[x2][y1-1]+partSum[x1-1][y1-1]).append("\n");
+		}
+		System.out.println(result); 
+	}
 }
