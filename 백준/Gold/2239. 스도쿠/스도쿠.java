66cd48b41d26b56e624e45e4f5717581b04374f6
HanJu <119592507+dev1week@users.noreply.github.com>
2023-09-26 18:40:14 +0900

    [Gold IV] Title: 스도쿠, Time: 624 ms, Memory: 20980 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -1,99 +1,80 @@
- 
+import java.util.Scanner;
 
-import java.io.BufferedReader;
-import java.io.IOError;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+public class Main {
+    static int[][] board = new int[9][9];
 
-public class Main  {
+    public static void main(String[] args) {
+        init(); // 스도쿠 초기화
+        solve(0, 0); // 스도쿠 풀이 시작
+    }
 
-	static StringBuilder sb;
-	static int[][] ans;
-	static ArrayList<int[]> list;
-	static boolean flag;
+    static void init() {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 9; i++) {
+            String line = scanner.next();
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = line.charAt(j) - '0'; // 문자열을 정수로 변환
+            }
+        }
+        scanner.close();
+    }
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		int N;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
+    static boolean isValid(int row, int col, int num) {
+        // 행과 열에 중복된 숫자가 있는지 확인
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
 
-//		char[][] arr = new char[9][9];
-		ans = new int[9][9];
-		list = new ArrayList<>();
+        // 3x3 박스에 중복된 숫자가 있는지 확인
+        int boxStartRow = row - (row % 3);
+        int boxStartCol = col - (col % 3);
+        for (int i = boxStartRow; i < boxStartRow + 3; i++) {
+            for (int j = boxStartCol; j < boxStartCol + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
 
-		for (int i = 0; i < 9; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < 9; j++) {
-				ans[i][j] = str.charAt(j) - '0';
-				if (ans[i][j] == 0)
-					list.add(new int[] { i, j });
-			}
-		}
+        return true;
+    }
 
-//
-//		for (int i = 0; i < list.size(); i++)
-//			System.out.println("list" + list.get(i)[0]);
-		solve(0);
-		System.out.println(sb.toString());
-	}
-// 
+    static void solve(int row, int col) {
+        if (row == 9) {
+            // 모든 행을 확인했으면 결과 출력
+            printBoard();
+            System.exit(0); // 첫 번째 답을 찾으면 프로그램 종료
+            return;
+        }
 
-	static void solve(int depth) {
+        if (board[row][col] == 0) {
+            // 현재 위치가 0이면 (빈 칸이면)
+            for (int num = 1; num <= 9; num++) {
+                if (isValid(row, col, num)) {
+                    // 숫자가 유효하면 해당 위치에 숫자를 넣고 다음 위치로 이동
+                    board[row][col] = num;
+                    int nextRow = col == 8 ? row + 1 : row;
+                    int nextCol = col == 8 ? 0 : col + 1;
+                    solve(nextRow, nextCol);
+                    board[row][col] = 0; // 백트래킹
+                }
+            }
+        } else {
+            // 현재 위치에 숫자가 이미 채워져 있으면 다음 위치로 이동
+            int nextRow = col == 8 ? row + 1 : row;
+            int nextCol = col == 8 ? 0 : col + 1;
+            solve(nextRow, nextCol);
+        }
+    }
 
-		if (depth == list.size()) {
-//			System.out.println("!!");
-
-			for (int i = 0; i < 9; i++) {
-				for (int j = 0; j < 9; j++) {
-					sb.append(ans[i][j]);
-				}
-				sb.append('\n');
-			}
-			flag = true;
-			return;
-		}
-
-		int r = list.get(depth)[0]; // list 에서 값을 꺼냈으므로 ans[r][c] 는 다 0 이다.
-		int c = list.get(depth)[1];
-
-		boolean[] visit = new boolean[10];
-		int nr = (r / 3) * 3;
-		int nc = (c / 3) * 3;
-
-		for (int i = nr; i < nr + 3; i++) { // 스도쿠에서 9등분 하여 네모난부분 .. 현재칸이 r,c 인데 여기에 다른 수랑 겹치면 안되니까 visit 을 true 로 해준다.
-			for (int j = nc; j < nc + 3; j++) {
-				int v = ans[i][j];
-				if (v != 0)
-					visit[v] = true;
-			}
-		}
-
-		for (int i = 0; i < 9; i++) {
-			int v = ans[r][i]; // 가로
-			if (v != 0)
-				visit[v] = true;
-			v = ans[i][c];// 세로
-			if (v != 0)
-				visit[v] = true;
-
-		}
-
-		for (int i = 1; i <= 9; i++) {
-
-			if (visit[i] == false) {
-				if (flag)
-					return;
-				ans[r][c] = i;
-//				System.out.println("r c" + r + " " + c + " ans " + ans[r][c]);
-				solve(depth + 1);
-				ans[r][c] = 0;
-
-			}
-
-		}
-
-	}
-
+    static void printBoard() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
