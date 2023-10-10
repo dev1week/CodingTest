f2f504ddee9206892a1b04743c537ec6299a65a0
HanJu <119592507+dev1week@users.noreply.github.com>
2023-10-10 21:31:59 +0900

    [Unrated] Title: [모의 SW 역량테스트] 무선 충전, Time: 127 ms, Memory: 24,248 KB -BaekjoonHub

--- SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
+++ SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
@@ -1,256 +1,210 @@
-import java.io.*; 
 
-import java.util.*; 
+import java.io.*;
+import java.util.*;
 
-
-class Point {
-	int x, y;
+class BC{
+	int x, y, c, p; 
 	
-	static final int[] dx = {0,-1,0,1,0};
-	static final int[] dy = {0,0,1,0,-1}; 
-	
-	public Point(int x, int y) {
-		this.x = x-1;
-		this.y = y-1;
+	public BC(int x, int y, int c, int p) {
+		this.x =x;
+		this.y = y; 
+		this.c = c; 
+		this.p = p; 
 	}
 	
-	public int getDistance(Point o) {
-		return Math.abs(this.x-o.x) + Math.abs(this.y-o.y); 
+	public String toString() {
+		return x+":"+y+"에서 범위는 "+c+" 충전량 " +p; 
+	}
+}
+
+class Player {
+	int x, y;
+	static final int[] dx = {0, -1, 0, 1, 0};
+	static final int[] dy = {0,  0, 1, 0, -1}; 
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y; 
 	}
 	
 	public String toString() {
 		return this.x+":"+this.y; 
 	}
 	
+	
 	public void move(int dir) {
-		int nX = this.x+ dx[dir];
-		int nY = this.y + dy[dir]; 
-		if(inRange(nX, nY)) {
-			this.x = nX;
-			this.y = nY; 
-		}
+		this.x =this.x + dx[dir];
+		this.y= this.y + dy[dir];
 	}
 	
-	static boolean inRange(int x, int y) {
-		return x>=0&&y>=0&&x<10&&y<10; 
+	
+	public boolean isAvailable(BC bc) {
+		return bc.c>= (Math.abs(bc.x-this.x)+Math.abs(bc.y-this.y));
 	}
-}
-
-class BC{
-	int[][] coverage;
-	int p;
-	
-	static final int[] dx = {-1,1,1,-1};
-	static final int[] dy = {-1,-1,1,1}; 
-	
-	public BC(int y, int x, int c, int p) {
-		this.coverage = new int[10][10]; 
-		this.p = p; 
-		//중심점부터 시작하여 대각선 굴리기 
-		this.coverage[x-1][y-1] = p; 
-		for(int length=1; length<=c; length++) {
-			int nX = x-1;
-			int nY = y+ length-1; 
-			for(int dir=0; dir<4; dir++) {
-				for(int l=0; l<length; l++ ) {
-					nX += dx[dir];
-					nY += dy[dir]; 
-					if(inRange(nX, nY)) {
-						this.coverage[nX][nY] = p; 
-					}
-				}
-			}
-		}
-	}
-	
-	static boolean inRange(int x, int y) {
-		return x>=0&&y>=0&&x<10&&y<10; 
-	}
-	
-	public String toString() {
-		StringBuilder sb = new StringBuilder(); 
-		
-		for(int x=0;x<10; x++) {
-			for(int y=0; y<10; y++) {
-				sb.append(" ").append(this.coverage[x][y]).append(" ");
-			}sb.append("\n");
-		}
-		
-		return sb.toString(); 
-	}
-	
 	
 }
 
-class Solution
-{	
-	static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in)); 
-	static StringTokenizer tokens; 
-	
-	//충전 가능 범위 
-		
-	
-	//거리 구하기 
-	
-	static int m; //총 이동 시간 
-	static int a; //bc의 개수 
-	static int sum; 
-	
-	static int[][] commands;
-	static int[] log; 
-	static boolean[][] isBCUsed; 
-	static Point[] people; 
-	static BC[] bcs; 
-	
-	
-	static final int UP =1;
-	static final int RIGHT = 2;
-	static final int DOWN = 3;
-	static final int LEFT =4; 
-	
-	static final int[] dx = {0,-1,0,1,0};
-	static final int[] dy = {0,0,1,0,-1}; 
-	
-	
-	static void init() throws IOException{
-		tokens = new StringTokenizer(buffer.readLine());
-		m = Integer.valueOf(tokens.nextToken());
-		a = Integer.valueOf(tokens.nextToken()); 
-		
-		people = new Point[2];
-		log = new int[m+1]; 
-		commands = new int[2][m+1];
-		bcs = new BC[a]; 
-		isBCUsed = new boolean[2][a];
-		for(int p=0; p<2; p++) {
-			tokens= new StringTokenizer(buffer.readLine()); 
-			for(int t=1; t<=m; t++) {
-				commands[p][t] = Integer.valueOf(tokens.nextToken()); 
-			}
-		}
-		
-		people[0] = new Point(1,1);
-		people[1] = new Point(10,10);
-		
-		for(int i=0; i<a; i++) {
-			tokens = new StringTokenizer(buffer.readLine());
-			bcs[i] = new BC(Integer.valueOf(tokens.nextToken()),
-					Integer.valueOf(tokens.nextToken()),
-					Integer.valueOf(tokens.nextToken()),
-					Integer.valueOf(tokens.nextToken()));
+public class Solution {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    
 
-		}
-		
-		
+    
+    //사용자의 초기 위치부터 충전 가능 
+    
+    //bc
+    	//위치 
+    	//충전 범위 (거리가 이하까지 가능 )
+    	//성능 
+    	//겹쳐 있으면 둘 중 하나만 접속 가능 
+    	//2명이 동에 접속한 경우 충전양 균등 분배 
+    	//같은 위치에 2개 이상 설치된 경우는 없음 
+    
+    
+    //a는 1,1 지점에서 출발 
+    //b는 10, 10 지점에서 출발 
+    
+    //두 지점 사이의 거리 = 맨해튼 거리
+    
+    static final int SIZE = 10; 
+    static int m; //~100 총 이동 시간 
+    static int A; // bc의 개수 
+    static Player a;
+    static Player b; 
+    static int[] commandsA;
+    static int[] commandsB; 
 
-	}
-	
-	static int[] selecteds;
-	static int[] userCounts; 
-	static void bt(int people) {
-		
-		if(people==2) {
-			int tmp = 0;
+    static BC[] bcs; 
+    
+    static int result; //모든 사용자의 충전량 합의 최댓값 
+    
+    static void init() throws IOException{
+    	tokens = new StringTokenizer(buffer.readLine()); 
+    	m = Integer.valueOf(tokens.nextToken()); 
+    	A = Integer.valueOf(tokens.nextToken()); 
+    	
+    	commandsA = new int[m]; 
+    	commandsB = new int[m]; 
 
-			for(int i=0; i<2; i++) {
-				if(userCounts[selecteds[i]]==0) {return;}
-				tmp += bcs[selecteds[i]].p/userCounts[selecteds[i]];
-			}
-			sum = Math.max(tmp,sum);
-			return; 
-		}
-		
-		for(int bcIdx=0; bcIdx<a; bcIdx++) {
-			if(isBCUsed[people][bcIdx]) {
-				selecteds[people] = bcIdx;
-				userCounts[bcIdx]++;
-				bt(people+1);
-				userCounts[bcIdx]--;
-			}
-		}
-	}
-	static int result; 
-	public static void main(String[] args)throws IOException{
-		int T = Integer.valueOf(buffer.readLine());
-		StringBuilder format = new StringBuilder(); 
-		for(int test=1; test<=T; test++) {
-			init();
-			
-			
-			for(int time=0; time<=m; time++) {
-				isBCUsed = new boolean[2][a]; 
-				
-				//이동 후 사용중인 BC 표시하기 
-				for(int person=0; person<2; person++) {
-					
-					//커맨드 따라 움직임
-					int command = commands[person][time]; 
-					people[person].move(command);
-					Point p = people[person]; 
-
-					for(int bcIdx = 0; bcIdx<a; bcIdx++) {
-	
-						if(bcs[bcIdx].coverage[p.x][p.y]>0) {
-							isBCUsed[person][bcIdx] = true; 
-						}
-					}
-				}
-				//최적의 BC 분배하기 
-				
-				boolean flag = false; 
-				for(int bcIdx=0; bcIdx<a; bcIdx++) {
-					int r = 0; 
-					for(int p=0; p<2; p++) {
-						if(isBCUsed[p][bcIdx]) {
-							r++;
-						}
-					}
-					if(r==2) {
-						flag = true;
-						break; 
-					}
-				}
-				
-				sum  =0; 
-				
-				if(flag) {
-					//같은거 2개 동시에 쓰는 경우에만 해당 
-					//백트래킹으로 2개 경우의 수 뽑기
-					selecteds = new int[2]; 
-					userCounts = new int[a]; 
-					bt(0);
-				}else {
-					for(int p=0; p<2; p++) {
-						int ind = 0; 
-						for(int bcIdx=0; bcIdx<a; bcIdx++) {
-							if(isBCUsed[p][bcIdx]) {
-								ind = Math.max(ind, bcs[bcIdx].p);
-							}
-						}
-						sum += ind; 
-					}
-				}
-//				System.out.println(Arrays.toString(people)); 
-//				System.out.println(flag);
-//				System.out.println(Arrays.toString(isBCUsed[0]));
-//				System.out.println(Arrays.toString(isBCUsed[1]));
-//				System.out.println(sum);
-				
-				//갱신 
-				log[time]= sum; 
-				result = 0;
-				for(int l : log) {
-					result += l; 
-				}
-			}
-
-			format.append("#").append(test).append(" ").append(result).append("\n"); 
-		}
-		
-		System.out.println(format); 
-		
-		
-	}
-	
-	
+    	
+    	bcs = new BC[A]; 
+    	tokens = new StringTokenizer(buffer.readLine());
+    	for(int time=0; time<m; time++) {
+    		commandsA[time] = Integer.valueOf(tokens.nextToken());
+    	}
+    	tokens = new StringTokenizer(buffer.readLine());
+    	for(int time=0; time<m; time++) {
+    		commandsB[time] = Integer.valueOf(tokens.nextToken());
+    	}
+    	
+    	
+    	for(int i=0; i<A; i++) {
+    		tokens = new StringTokenizer(buffer.readLine()); 
+    		int y= Integer.valueOf(tokens.nextToken());
+    		int x= Integer.valueOf(tokens.nextToken());
+    		int c= Integer.valueOf(tokens.nextToken());
+    		int p= Integer.valueOf(tokens.nextToken());
+    		bcs[i] = new BC(x,y,c,p); 
+    	}
+    	
+    	
+    	
+    	a = new Player(1,1);;
+    	b = new Player(10,10);
+    }
+    
+    static ArrayList<BC> getBCList(Player p){
+    	
+    	ArrayList<BC> possibles = new ArrayList<>(); 
+    	
+    	for(BC bc : bcs) {
+    		if(p.isAvailable(bc)) {
+    			possibles.add(bc); 
+    		}
+    	}
+    	
+    	return possibles; 
+    	
+    	
+    }
+    
+    public static void main(String[] args)throws IOException{
+    	int T = Integer.valueOf(buffer.readLine());
+    	
+    	StringBuilder sb = new StringBuilder(); 
+    	
+    	for(int t=1; t<=T; t++) {
+    		init(); 
+    		int result = 0;
+    		for(int time=0; time<=m; time++) {
+    			//a의 현재 위치에서 접속 가능한 bc 뽑기 
+    			ArrayList<BC> possibleToA = getBCList(a);
+    			//b의 현재 위치에서 접속 가능한 bc 뽑기 
+    			ArrayList<BC> possibleToB = getBCList(b);
+    			
+    			result += getPower(possibleToA, possibleToB); 
+    			
+    			//a와 b 리스트로 가능한 조합 뽑기 
+    				//현재 시간에서의 최대 충전량 구하기 
+    				//a==0일 때 
+    				//b==0일 때 
+    				//둘다 0일 때 
+    				//둘다 0이 아닐 때 
+    			
+    			//배열에 넣어주기 
+    			
+    			//a 이동 시키기 
+    			if(time!=m) {
+    				a.move(commandsA[time]);
+        			//b 이동 시키기 
+        			b.move(commandsB[time]);
+    			}
+    			
+    		}
+    		
+    		//로그 배열에서 충전량 총합 구하기 
+    		
+    		sb.append("#").append(t).append(" ").append(result).append("\n");
+    	}
+    	System.out.println(sb);
+    }
+    
+    
+    static int getPower(ArrayList<BC> aList, ArrayList<BC> bList) {
+    	int totalPower = 0; 
+    	if(aList.size()==0&&bList.size()==0) {
+    		return 0; 
+    	}
+    	//둘다 없을 경우 
+    	else if(bList.size()==0) {
+    		for(BC aSelected : aList) {
+    			totalPower = Math.max(aSelected.p, totalPower);
+    		}
+    		return totalPower; 
+    	}
+    	//aList가 0일 경우 
+    	else if(aList.size()==0) {
+    		for(BC bSelected : bList) {
+    			totalPower = Math.max(bSelected.p, totalPower);
+    		}
+    		return totalPower; 
+    	}else {
+    		for(BC aSelected : aList) {
+    			for(BC bSelected : bList) {
+    				if(aSelected == bSelected) {
+    					totalPower = Math.max(totalPower, aSelected.p);
+    				}else {
+    					totalPower = Math.max(totalPower, aSelected.p + bSelected.p);
+    				}
+    			}
+    		}
+    		return totalPower; 
+    	}
+    	//bList가 0일 경우 
+    	
+    	//둘다 0이 아닐 경우 
+    }
+   
+    
+    	
 }
