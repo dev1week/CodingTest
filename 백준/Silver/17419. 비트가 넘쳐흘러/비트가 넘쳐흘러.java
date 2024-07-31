051b3d9c01c618febaa2ab8a44148457c5df323d
HanJu <119592507+dev1week@users.noreply.github.com>
2024-07-31 15:11:48 +0900

    [Silver IV] Title: 비트가 넘쳐흘러, Time: 268 ms, Memory: 20076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.java
+++ 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.java
@@ -7,28 +7,18 @@ public class Main {
     static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer tokens;
 
-    private static int operate(int data){
-        return data-(data&((~data)+1));
-    }
-
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(buffer.readLine());
 
         String binary = buffer.readLine();
-
-        int data = Integer.parseInt(binary,2);
-
-        int count  = 0;
-        while(data>0){
-            data = operate(data);
-            count ++;
+        long count = 0;
+        for(int i=0; i<n; i++){
+            if(binary.charAt(i)=='1'){
+                count++;
+            }
         }
-
         System.out.println(count);
-
-
-
     }
 
 }
